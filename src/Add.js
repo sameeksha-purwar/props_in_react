9e3a053dc4418244e3935af1101843046b77350a
Sameeksha purwar <69745044+sameeksha-purwar@users.noreply.github.com>
2023-03-03 11:27:59 +0530

    Add files via upload

---
+++ src/Add.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+    function Add(num1,num2){
+        return num1+num2
+    }
+    
+   
+export default Add;
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "./head";
+import Add from "./Add";
+function App() {
+  return (
+    <div>
+    <Head/>
+    {Add(2,3)}
+   
+
+    </div>
+
+  );
+  
+}
+
+export default App;
